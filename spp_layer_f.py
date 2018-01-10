02809e9e1dbf88e1c9b3b1dac2e9d712cfa98332
1900sala <15652700190@163.com>
2018-01-10 09:59:58 +0800

    '1'

--- spp_layer_f.py
+++ spp_layer_f.py
@@ -9,10 +9,10 @@ import math
 
 class SPPLayer():
     def __init__(self,bins,feature_map_size):
-        self.strides = []
-        self.filters = []
+        self.h = []
+        self.w = []
 #        print(type(feature_map_size))
-        self.a = float(feature_map_size)
+        self.feature_map_size = feature_map_size
         self.bins = bins
         self.n = len(bins)
 
@@ -20,24 +20,26 @@ class SPPLayer():
         self.input = data
         self.map_nums=self.input.get_shape().as_list()[3]
         for i in range(self.n):
-            x = int(math.floor(self.a/float(self.bins[i])))
-            self.strides.append(x)
-            x = int (math.ceil(self.a/float(self.bins[i])))
-            self.filters.append(x)  
-        print('bin_strides', self.strides)
-        print('bin_filters', self.filters)
-        
+            x = math.ceil(self.feature_map_size[0]/float(self.bins[i]))
+            self.h.append(x)
+            x = math.ceil(self.feature_map_size[1]/float(self.bins[i]))
+            self.w.append(x)
+
+
         self.pooled_out = []
         for i in range(self.n):
             self.pooled_out.append(tf.nn.max_pool(self.input,
-                ksize=[1, self.filters[i], self.filters[i], 1], 
-                strides=[1, self.strides[i], self.strides[i], 1],
-                padding='VALID'))
+                ksize=[1, self.h[i], self.w[i], 1],
+                strides=[1, self.h[i], self.w[i], 1],
+                padding='SAME'))
+        #print('pooled_out', self.pooled_out)
         for i in range(self.n):
+            #print(self.bins[i], self.bins[i]*self.bins[i]*self.map_nums)
+            #print(self.pooled_out[i].shape)
             self.pooled_out[i] = tf.reshape(self.pooled_out[i], [-1, self.bins[i]*self.bins[i]*self.map_nums])
-            
-        print('pooled_out', self.pooled_out)
+
+        #print('pooled_out', self.pooled_out)
         self.output = tf.concat( [self.pooled_out[0], self.pooled_out[1], self.pooled_out[2]],1)
-        print(self.output.shape)
+        #print(self.output.shape)
         return self.output
 
--- spp_net_f.py
+++ spp_net_f.py
@@ -56,56 +56,52 @@ class SPPnet:
         if tp is not None:
             with tf.name_scope('SPP'):
                 print('**********SPP*************')
-                self.conv1 = self._conv_layer(data, 'conv1', [5, 5, 1, 6])
+                self.conv1 = self._conv_layer(data, 'conv1', [5, 5, 1, 16])
                 self.pool1 = tf.nn.max_pool(self.conv1, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],
                     padding='SAME',name='pool1')
                 print ('pool1.shape', self.pool1.shape)
 
-                self.conv2 = self._conv_layer(self.pool1, 'conv2', [5, 5, 6, 16])
-                self.conv3 = self._conv_layer(self.conv2, 'conv3', [5, 5, 16, 16])
-      
+                self.conv2 = self._conv_layer(self.pool1, 'conv2', [5, 5, 16, 16])
+                self.pool2 = tf.nn.max_pool(self.conv2, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],
+                    padding='SAME',name='pool2')
+                print ('pool2.shape', self.pool2.shape)
+
                 bins = [ 3, 2, 1]
-                map_size = self.conv3.get_shape().as_list()[2]
-                print('conv3.shape', self.conv3.get_shape())
+                map_size = self.pool2.get_shape().as_list()[1:3]
+                print('conv3.shape', self.pool2.get_shape())
                 sppLayer = SPPLayer(bins, map_size)
-                self.sppool = sppLayer.spatial_pyramid_pooling(self.conv3)
+                self.sppool = sppLayer.spatial_pyramid_pooling(self.pool2)
             
                 numH = self.sppool.get_shape().as_list()[1]
                 print('numH', numH)
-                self.fc7 = self._fc_layer(self.sppool, 'fc6', shape=[numH, 48])
+                self.fc7 = self._fc_layer(self.sppool, 'fc6', shape=[numH, 256])
                 if train:
                     self.fc7 = tf.nn.dropout(self.fc7, 0.5, seed=SEED)
-                self.output = self._fc_layer(self.fc7, 'output', shape=[48,num_class])
+                self.output = self._fc_layer(self.fc7, 'output', shape=[256,num_class])
                 print('inference')
                 return self.output
         else:
             with tf.name_scope('cnn'):
                 print('**********CNN*************')
-                self.conv1 = self._conv_layer(data, 'conv1', [5,5,1,6])
+                self.conv1 = self._conv_layer(data, 'conv1', [5,5,1,16])
                 self.pool1 = tf.nn.max_pool(self.conv1, ksize=[1,2,2,1],strides=[1,2,2,1],
                     padding='SAME',name='pool1')
                 print ('pool1.shape', self.pool1.shape)
                  
-                self.conv2 = self._conv_layer(self.pool1, 'conv2', [5,5,6,16])
+                self.conv2 = self._conv_layer(self.pool1, 'conv2', [5,5,16,16])
                 self.pool2 = tf.nn.max_pool(self.conv2, ksize=[1,2,2,1],strides=[1,2,2,1],
                     padding='SAME',name='pool2')
                 print ('pool2.shape', self.pool2.shape)
                 
-                self.conv3 = self._conv_layer(self.pool2, 'conv3', [5,5,16,16])
-                self.pool3 = tf.nn.max_pool(self.conv3, ksize=[1,2,2,1],strides=[1,2,2,1],
-                    padding='SAME',name='pool3')
-                print ('pool3.shape', self.pool3.shape)
-                
- 
-                temp1, temp2, temp3 = self.pool3.get_shape().as_list()[1:4]
-                self.fc = tf.reshape(self.pool3, [-1, temp1*temp2*temp3])
+
+                temp1, temp2, temp3 = self.conv2.get_shape().as_list()[1:4]
+                self.fc = tf.reshape(self.conv2, [-1, temp1*temp2*temp3])
                 print ('fc.shape', self.fc.shape)
-                
-                self.fc1 = self._fc_layer(self.fc, 'fc1',shape= [temp1*temp2*temp3,32])
-                
+
+                self.fc1 = self._fc_layer(self.fc, 'fc1',shape= [temp1*temp2*temp3,256])
                 if train:
                     self.fc1 = tf.nn.dropout(self.fc1, 0.5, seed=SEED)
-                self.output = self._fc_layer(self.fc1, 'output', shape=[32,num_class])
+                self.output = self._fc_layer(self.fc1, 'output', shape=[256,num_class])
                 print('inference')
                 return self.output
     
@@ -115,9 +111,7 @@ class SPPnet:
             self.pred = tf.nn.softmax(logits)
             if label is not None:
                 label = tf.cast(label, tf.float32)
-                self.entropy_loss = -tf.reduce_mean(label * tf.log(tf.clip_by_value(self.pred,1e-5,1)))  
-                self.lr = tf.train.exponential_decay(self.lr, global_step*self.batch_size, self.train_size*self.decay_epochs, 0.95, staircase=True)
-                #self.optimizer = tf.train.MomentumOptimizer(self.lr, 0.1).minimize(self.entropy_loss,global_step = global_step)
+                self.entropy_loss = -tf.reduce_mean(label * tf.log(tf.clip_by_value(self.pred,1e-5,1)))
                 self.optimizer = tf.train.AdamOptimizer(1e-4).minimize(self.entropy_loss)
                 correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(label,1))
                 self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
